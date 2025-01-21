d1fa7b003c2267e93f01d2af056c0f6bfb420ae9
quim-romero <quimromero22@gmail.com>
2025-01-21 12:15:00 +0100

    Feat: Add ScrollToTop to main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
+
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
-      <Route path="/gallery" element={<h1>Gallery</h1>} />
-      <Route path="/favorites" element={<h1>Favorites</h1>} />
-      <Route path="/about" element={<h1>About</h1>} />
-      <Route path="/contact" element={<h1>Contact</h1>} />
-      <Route path="*" element={<h1>Not Found</h1>} />
-    </Routes>
-  )
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/gallery" element={<h1>Gallery</h1>} />
+        <Route path="/favorites" element={<h1>Favorites</h1>} />
+        <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/contact" element={<h1>Contact</h1>} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </>
+  );
 }
