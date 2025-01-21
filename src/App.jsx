46b0d26643e5dd06d0b5fbdee413e5b1a2b0b34e
quim-romero <quimromero22@gmail.com>
2025-01-21 09:50:00 +0100

    Feat: Add React Router and configure main routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/gallery" element={<h1>Gallery</h1>} />
+      <Route path="/favorites" element={<h1>Favorites</h1>} />
+      <Route path="/about" element={<h1>About</h1>} />
+      <Route path="/contact" element={<h1>Contact</h1>} />
+      <Route path="*" element={<h1>Not Found</h1>} />
+    </Routes>
   )
 }
-
-export default App
