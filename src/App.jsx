2be6ff6794cdba7a005954d891008673c8ef27c6
quim-romero <quimromero22@gmail.com>
2025-01-25 11:30:00 +0100

    Feat: Add route for /favorites to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home"
 import Gallery from "./pages/Gallery"
+import Favorites from "./pages/Favorites"
 
 import ScrollToTop from "./components/ScrollToTop";
 import Navbar from "./components/Navbar";
@@ -13,7 +14,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/gallery" element={<Gallery/>} />
-        <Route path="/favorites" element={<h1>Favorites</h1>} />
+        <Route path="/favorites" element={<Favorites/>} />
         <Route path="/about" element={<h1>About</h1>} />
         <Route path="/contact" element={<h1>Contact</h1>} />
         <Route path="*" element={<h1>Not Found</h1>} />
