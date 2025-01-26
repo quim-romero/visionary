c6c330f3d84c547c51d62783822528ab536112f2
quim-romero <quimromero22@gmail.com>
2025-01-26 10:30:00 +0100

    Feat: Add About route to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home"
 import Gallery from "./pages/Gallery"
 import Favorites from "./pages/Favorites"
+import About from "./pages/About"
 
 import ScrollToTop from "./components/ScrollToTop";
 import Navbar from "./components/Navbar";
@@ -16,7 +17,7 @@ export default function App() {
         <Route path="/" element={<Home/>} />
         <Route path="/gallery" element={<Gallery/>} />
         <Route path="/favorites" element={<Favorites/>} />
-        <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/about" element={<About/>} />
         <Route path="/contact" element={<h1>Contact</h1>} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
