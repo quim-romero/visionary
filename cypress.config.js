de9b1bea2eff7197b3f3831a5fd435c3dbfe23f2
quim-romero <quimromero22@gmail.com>
2025-02-04 17:30:00 +0100

    test(config): cypress.config.js with baseUrl per environment

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:5173",
-    setupNodeEvents() {
-    },
+    baseUrl: process.env.CYPRESS_BASE_URL || "http://localhost:5173",
+    supportFile: "cypress/support/e2e.js",
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    video: false,
+    screenshotOnRunFailure: true,
+    chromeWebSecurity: false,
   },
 });
